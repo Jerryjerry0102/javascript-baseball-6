16b2beb3fe95534045cb743ada28dbdf9ebc575c
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-10-25 23:23:10 +0900

    내가 할 수 있는 최선

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import NumberBaseball from "./NumberBaseball.js";
+import NumberBaseballConsole from "./NumberBaseballConsole.js";
+import NumberBaseballUmpire from "./NumberBaseballUmpire.js";
+import NumberBaseballUmpireIndicator from "./NumberBaseballUmpireIndicator.js";
+import RandomNumbersGenerator from "./RandomNumbersGenerator.js";
+
 class App {
-  async play() {}
+  async play() {
+    const numberBaseball = new NumberBaseball(
+      new NumberBaseballConsole(1, 9, 3, 1, 2),
+      new NumberBaseballUmpire(new NumberBaseballUmpireIndicator(3)),
+      new RandomNumbersGenerator(1, 9, 3)
+    );
+    await numberBaseball.start();
+  }
 }
+
+const app = new App(); // 개발용
+app.play(); // 개발용
 
 export default App;
